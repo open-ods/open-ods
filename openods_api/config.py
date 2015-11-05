6ae061a3343b99a63424cc03e5a749709545e780
Matt Stibbs <git@stibbsy.co.uk>
2015-11-05 16:23:52 +0000

    Add default hostname and port for running locally

--- openods_api/config.py
+++ openods_api/config.py
@@ -2,7 +2,7 @@ import os
 
 DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://dev:dev@localhost:5432/ods')
 CACHE_TIMEOUT = int(os.environ.get('CACHE_TIMEOUT', '10'))
-APP_HOSTNAME = os.environ.get('APP_HOSTNAME', 'localhost')
+APP_HOSTNAME = os.environ.get('APP_HOSTNAME', 'localhost:5000')
 
 print(str.format("Database URL: {0}", DATABASE_URL))
 print(str.format("Cache Timeout: {0}", CACHE_TIMEOUT))
