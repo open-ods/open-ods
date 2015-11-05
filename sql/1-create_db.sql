9daa57c03aee5da60ad804fc95db9392e93f9bc6
Matt Stibbs <git@stibbsy.co.uk>
2015-11-05 20:28:08 +0000

    Add comments to SQL scripts

--- sql/1-create_db.sql
+++ sql/1-create_db.sql
@@ -1,3 +1,4 @@
+-- You will need to do the following as the Postgres user (or another superuser)
 CREATE ROLE dev WITH PASSWORD 'dev' LOGIN;
 
 -- Database: ods
@@ -9,5 +10,7 @@ CREATE DATABASE ods
        LC_CTYPE = 'en_US.UTF-8'
        CONNECTION LIMIT = -1;
 
+
+-- Make sure you are connected to the new database you just created before installing these extensions
 CREATE EXTENSION "uuid-ossp";
 CREATE EXTENSION "adminpack";
