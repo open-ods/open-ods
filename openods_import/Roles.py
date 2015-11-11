66d18155937d52b9931fd1a161c59c3d1f9a4ead
tonyyates <tonyyates@gmail.com>
2015-11-11 17:11:41 +0000

    refactored roles to add additional fields to the class

--- openods_import/Roles.py
+++ openods_import/Roles.py
@@ -14,16 +14,41 @@ class Roles(Base):
     """
     __tablename__ = 'roles'
 
-    version_ref = Column(String)
+    role_ref = Column(String)
     organisation_ref = Column(String)
     org_odscode = Column(String)
     role_code = Column(String)
+    primary_role = Column(String)
+    role_unique_id = Column(String)
+    role_status = Column(String)
+    role_legal_start_date = Column(String)
+    role_legal_end_date = Column(String)
+    role_operational_start_date = Column(String)
+    role_operational_end_date = Column(String)
 
     # TODO: implement so a return is formatted nicely
     def __repr__(self):
-        return "<Roles(ref='%s', organisation_ref='%s',\
-            org_odscode='%s', role_code='%s')>" % (
-            self.version_ref,
-            self.organisation_ref,
-            self.org_odscode,
-            self.role_code)
+        return "<Roles(\
+            role_ref='%s', \
+            organisation_ref='%s', \
+            org_odscode='%s', \
+            role_code='%s', \
+            primary_role='%s', \
+            role_unique_id='%s', \
+            role_status='%s', \
+            role_legal_start_date='%s', \
+            role_legal_end_date='%s', \
+            role_operational_start_date='%s', \
+            role_operational_end_date='%s' \
+            )>" % (
+            role_ref,
+            organisation_ref,
+            org_odscode,
+            role_code,
+            primary_role,
+            role_unique_id,
+            role_status,
+            role_legal_start_date,
+            role_legal_end_date,
+            role_operational_start_date,
+            role_operational_end_date)
