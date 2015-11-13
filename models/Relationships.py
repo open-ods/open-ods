39ee0bc990428a0fe64613a3a1b1cbb58fc9e89f
tonyyates <tonyyates@gmail.com>
2015-11-13 12:24:15 +0000

    cleaned up relationships model and variable names

--- models/Relationships.py
+++ models/Relationships.py
@@ -14,23 +14,30 @@ class Relationships(Base):
     """
     __tablename__ = 'relationships'
 
-    version_ref = Column(String, primary_key = True)
-    organisation_ref = Column(String)
-    org_odscode = Column(String)
-    target_odscode = Column(String)
-    relationship_code = Column(String)
+    ref = Column(Integer, primary_key=True)
+    organisation_ref = Column(Integer)
+    code = Column(String(10))
+    target_odscode = Column(String(10))
+    org_odscode = Column(String(10))
+    legal_start_date = Column(DateTime)
+    legal_end_date = Column(DateTime)
+    operational_start_date = Column(DateTime)
+    operational_end_date = Column(DateTime)
+    status = Column(String(10))
+    uid = Column(String)
 
     # Returns a printable version of the objects contents
     def __repr__(self):
-        return "<Relationships(\
-            ref='%s',\
-            organisation_ref='%s',\
-            org_odscode='%s',\
-            target_odscode='%s',\
-            relationship_code='%s'\
+        return "<Relationships('%s %s %s %s %s %s %s %s %s %s %s'\
             )>" % (
-            self.version_ref,
+            self.ref,
             self.organisation_ref,
-            self.org_odscode,
+            self.relationship_code,
             self.target_odscode,
-            self.relationship_code)
+            self.org_odscode,
+            self.legal_start_date,
+            self.legal_end_date,
+            self.operational_start_date,
+            self.operational_end_date,
+            self.status,
+            self.uid)
