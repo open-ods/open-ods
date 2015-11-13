385b6ce2eaefc16962dea09261b1471fd57cb562
tonyyates <tonyyates@gmail.com>
2015-11-13 11:25:57 +0000

    Models, Controller and Addresses have been added
    I have moved the models into a model folder so we can use them for reads later.
    I have also created a blank database controller to start working on, this does
    nothing at the minute.
    I have added the addresses model, this is going to break the reads as I have
    renamed some of the table columns but it won't matter when we refactor the
    front end to use these models too.

---
+++ models/Addresses.py
@@ -0,0 +1,40 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class Addresses(Base):
+    """
+    Addressess class that keeps track of information about a
+    particular Addresses. This class uses SQLAlchemy as an ORM
+
+    """
+    __tablename__ = 'addresses'
+
+    addresses_ref = Column(Integer, primary_key=True)
+    organisation_ref = Column(Integer, ForeignKey("organisations.organisation_ref"))
+    org_odscode = Column(String(10))
+    street_address_line1 = Column(String)
+    street_address_line2 = Column(String)
+    street_address_line3 = Column(String)
+    town = Column(String)
+    county = Column(String)
+    postal_code = Column(String)
+    location_id = Column(String)
+
+    # Returns a printable version of the objects contents
+    def __repr__(self):
+        return "<Addresses(%s %s %s %s %s %s %s %s %s \)>" \% (
+            self.addresses_ref
+            self.organisation_ref
+            self.org_odscode
+            self.street_address_line1
+            self.street_address_line2
+            self.street_address_line3
+            self.town
+            self.county
+            self.postal_code
+            self.location_id)
