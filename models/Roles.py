0d0cda125df88f45c4dfb4dd9bd5799a4db6d8c6
tonyyates <tonyyates@gmail.com>
2015-11-13 12:37:28 +0000

    cleaned up roles model and variable names

--- models/Roles.py
+++ models/Roles.py
@@ -14,41 +14,29 @@ class Roles(Base):
     """
     __tablename__ = 'roles'
 
-    role_ref = Column(Integer, primary_key=True)
+    ref = Column(Integer, primary_key=True)
     organisation_ref = Column(String)
     org_odscode = Column(String(10))
-    role_code = Column(String(10))
+    code = Column(String(10))
     primary_role = Column(Boolean)
-    role_unique_id = Column(String(10))
-    role_status = Column(String(10))
-    role_legal_start_date = Column(DateTime)
-    role_legal_end_date = Column(DateTime)
-    role_operational_start_date = Column(DateTime)
-    role_operational_end_date = Column(DateTime)
+    unique_id = Column(String(10))
+    status = Column(String(10))
+    legal_start_date = Column(DateTime)
+    legal_end_date = Column(DateTime)
+    operational_start_date = Column(DateTime)
+    operational_end_date = Column(DateTime)
 
     # Returns a printable version of the objects contents
     def __repr__(self):
-        return "<Roles(\
-            role_ref='%s', \
-            organisation_ref='%s', \
-            org_odscode='%s', \
-            role_code='%s', \
-            primary_role='%s', \
-            role_unique_id='%s', \
-            role_status='%s', \
-            role_legal_start_date='%s', \
-            role_legal_end_date='%s', \
-            role_operational_start_date='%s', \
-            role_operational_end_date='%s' \
-            )>" % (
-            role_ref,
+        return "<Roles(' %s %s %s %s %s %s %s %s %s %s %s')>" % (
+            ref,
             organisation_ref,
             org_odscode,
-            role_code,
+            code,
             primary_role,
-            role_unique_id,
-            role_status,
-            role_legal_start_date,
-            role_legal_end_date,
-            role_operational_start_date,
-            role_operational_end_date)
+            unique_id,
+            status,
+            legal_start_date,
+            legal_end_date,
+            operational_start_date,
+            operational_end_date)
