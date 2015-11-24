0816d66d0f7ebe802b22c700c0e07d52ada77433
tonyyates <tonyyates@gmail.com>
2015-11-24 09:08:57 +0000

    Unit test and validated ODSFileManager imports
    ODSFileManger is now unit tested, we validate that an object is present and
    the value is no longer None.

--- controller/DataBaseSetup.py
+++ controller/DataBaseSetup.py
@@ -1,48 +1,38 @@
 from lxml import etree as xml_tree_parser
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+import models.*
 
 
 import os.path
 import sys
 import zipfile
 
-Base = declarative_base()
+engine = create_engine('sqlite:///openods.db', echo=True)
+Base = declarative_base(engine)
+metadata = Base.metadata
 
 
 class DataBaseSetup(object):
 
     __ods_xml_data = None
-    __database_connection = None
 
-    def __init__(self, database_connection):
-        self.__database_connection = database_connection
-
-    def __return_attribute(self, attribute_name):
+    def __init__(self):
         pass
 
-    # The purpose of this function is to retrieve the latest published
-    # file from a public published location
-    def __retrieve_latest_datafile(self):
-        # TODO: Retrieve latest file from the local directory until
-        # such time it is published and retrievable
-        if os.path.isfile('odsfull.xml.zip'):
-            return "odsfull.xml.zip"
-        else:
-            raise ValueError("unable to locate the data file")
+    def create_addresses(self):
 
-    # The purpose of this function is to determine if we have a zip
-    # for or xml file, check it is valid
-    # and then populate an etree object for us to parse
-    # TODO: validate the xml file against a schema
-    def __import_latest_datafile(self, data_filename):
+        address = Addresses()
 
-        try:
-            with zipfile.ZipFile(data_filename) as local_zipfile:
-                data_filename = data_filename.replace("zip", "")
-                with local_zipfile.open(data_filename) as local_datafile:
-                    self.__ods_xml_data = xml_tree_parser.parse(local_datafile)
-                return True
-        except:
-            print("Unexpected error:", sys.exc_info()[0])
-            raise
+        address.organisation_ref = '123test'
+        address.org_odscode = Column('123test'
+        address.street_address_line1 = '123test'
+        address.street_address_line2 = '123test'
+        address.street_address_line3 = '123test'
+        address.town = '123test'
+        address.county = '123test'
+        address.postal_code = '123test'
+        address.location_id = '123test'
+
+    def create_database(self):
+        pass
--- test/test_ODSFileManager.py
+++ test/test_ODSFileManager.py
@@ -1,13 +1,20 @@
+from distutils import file_util
+from lxml import etree as xml_tree_parser
+import logging
 import os.path
+import os.path
+import sys
 import sys
 import unittest
-import sys
-import os.path
-from distutils import file_util
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from controller.ODSFileManager import ODSFileManager
+
+# Logging Setup
+log = logging.getLogger('import_ods_xml')
+log.setLevel(logging.DEBUG)
+
 
 File_manager = ODSFileManager()
 Src_file = 'data/odsfull.xml.zip'
@@ -26,8 +33,11 @@ class ods_file_manager_test(unittest.TestCase):
         file_util.copy_file(Src_file,
                             Dst_file, update=True)
 
-        __ods_xml_data__ = File_manager.get_latest_xml()
-        self.assertIs(None, self.__ods_xml_data, False)
+        self.__ods_xml_data = File_manager.get_latest_xml()
+        self.assertTrue(self.__ods_xml_data)
+        log.info(self.__ods_xml_data
+                 .find('./Manifest/Version')
+                 .attrib.get('value'))
 
     def test_newer_remote_file_available(self):
         pass
