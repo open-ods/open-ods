73418f88af95354fa0a6e0cc0bc8428c5c57f9a9
tonyyates <tonyyates@gmail.com>
2015-11-26 13:05:50 +0000

    log total time taken and improved comments
    The total time taken for the entire script is now logged.
    Added more DOCSTRING comments in the ODSFileManager.

--- controller/ODSFileManager.py
+++ controller/ODSFileManager.py
@@ -22,9 +22,18 @@ class ODSFileManager(object):
     def __return_attribute(self, attribute_name):
         pass
 
-    # The purpose of this function is to retrieve the latest published
-    # file from a public published location
     def __retrieve_latest_datafile(self):
+        """The purpose of this function is to retrieve the latest
+        published file from a public published location
+
+        Parameters
+        ----------
+        None
+        Returns
+        -------
+        String: Filename if found
+        """
+
         # TODO: Retrieve latest file from the local directory until
         # such time it is published and retrievable
         if os.path.isfile('controller/odsfull.xml.zip'):
@@ -37,6 +46,16 @@ class ODSFileManager(object):
     # and then populate an etree object for us to parse
     # TODO: validate the xml file against a schema
     def __import_latest_datafile(self, data_filename):
+        """The purpose of this function is to determine if we have a zip
+        for or xml file, check it is valid
+
+        Parameters
+        ----------
+        String: filename of the zip file containing the xml
+        Returns
+        -------
+        None
+        """
 
         try:
             with zipfile.ZipFile(data_filename) as local_zipfile:
@@ -48,16 +67,22 @@ class ODSFileManager(object):
 
                 with local_zipfile.open(data_filename) as local_datafile:
                     self.__ods_xml_data = xml_tree_parser.parse(local_datafile)
-
-                return self.__ods_xml_data
         except:
             print('Unexpected error:', sys.exc_info()[0])
             raise
 
-    # The purpose of this function is to check if we have odsxml data
-    # if we don't it should retrieve the latest version available and
-    # explode it from zip format into a xmltree object
     def get_latest_xml(self):
+        """The purpose of this function is to check if we have odsxml data
+        if we don't it should retrieve the latest version available and
+        explode it from zip format into a xmltree object
+
+        Parameters
+        ----------
+        None
+        Returns
+        -------
+        xml_tree_parser: containing the entire xml dataset
+        """
 
         if self.__ods_xml_data is None:
             data_filename = self.__retrieve_latest_datafile()
