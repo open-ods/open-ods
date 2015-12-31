989c6df2b6ada04f5360d8aa2d6ec32a464439a6
Matt Stibbs <git@stibbsy.co.uk>
2015-12-31 19:06:24 +0000

    Update tests

--- test/test_canary.py
+++ test/test_canary.py
@@ -1,5 +1,4 @@
 import unittest
-import openods_core
 
 
 class CanaryTests(unittest.TestCase):
--- test/test_openods_api_config.py
+++ test/test_openods_api_config.py
@@ -1,17 +1,18 @@
 import unittest
-import openods_core.config as config
+
+from app import app
 
 
 class ConfigTests(unittest.TestCase):
 
     def test_app_hostname_is_not_none(self):
-        value = config.APP_HOSTNAME
+        value = app.config['APP_HOSTNAME']
         self.assertIsNotNone(value)
 
     def test_cache_timeout_is_greater_equal_0(self):
-        value = config.CACHE_TIMEOUT
+        value = app.config['CACHE_TIMEOUT']
         self.assertGreaterEqual(value, 0)
 
     def test_database_url_is_not_none(self):
-        value = config.DATABASE_URL
+        value = app.config['DATABASE_URL']
         self.assertIsNotNone(value)
