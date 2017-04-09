8f31448330a364413f03b218b99bf169aac55c42
Matt Stibbs <git@stibbsy.co.uk>
2017-04-09 23:12:11 +0100

    Minor tweaks

--- openods/__init__.py
+++ openods/__init__.py
@@ -13,13 +13,12 @@ app = Flask(__name__)
 feature_flags = FeatureFlag(app)
 
 # Load the app configuration from the default_config.py file
-app.config.from_pyfile('default_config.py')
 app.config.from_object('openods.default_config')
 
 from openods.openods_core import routes
 
 # Set up logging
-log_format = "%(asctime)s %(levelname)s %(message)s"
+log_format = "%(asctime)s [%(levelname)s] %(message)s"
 formatter = logging.Formatter(log_format)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG) if app.config["DEBUG"] is True else logger.setLevel(logging.INFO)
@@ -27,12 +26,14 @@ ch = logging.StreamHandler()
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
+logger.debug("Logging at DEBUG level")
+
 # Allow Cross Origin Resource Sharing for routes under the API path so that other services can use the API
 regEx=re.compile(app.config['API_URL'] + "/*")
 CORS(app, resources={regEx: {"origins": "*"}})
 
 # Import and register blueprints
-from openods.openods_site.controllers import mod_site as site_module
-app.register_blueprint(site_module)
+from openods.openods_site.controllers import mod_site
+app.register_blueprint(mod_site)
 
 
