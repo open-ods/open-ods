7361a7b72c81fc47562cbbd48bebb516e7046573
Matt Stibbs <git@stibbsy.co.uk>
2017-06-08 18:09:06 +0100

    Remove retry from main DB connection routine as schema_check verifies on startup

--- openods/connection.py
+++ openods/connection.py
@@ -6,14 +6,12 @@ import psycopg2
 import psycopg2.extras
 import psycopg2.pool
 from flask import g
-from retrying import retry
 
 from openods import app
 
 url = urlparse(app.config['DATABASE_URL'])
 
 
-@retry(wait_exponential_multiplier=1000, wait_exponential_max=10000)
 def get_connection():
     try:
         conn = psycopg2.connect(
